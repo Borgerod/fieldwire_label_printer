662ef2eade7b134e096d5891abb4407b335fec8c
Aleksander Borgerød <97392841+Borgerod@users.noreply.github.com>
2024-06-01 13:33:33 +0200

    Update backend / app.py
    NOTE: this is a sort of quicksave before i make some structure changes to backend;
    will split up app into smaller parts

--- download_component_labels/src/backend/app.py
+++ download_component_labels/src/backend/app.py
@@ -1,46 +1,70 @@
-# backend/app.py
+''' external imports '''
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+from datetime import datetime as dt
 import aiohttp
 import asyncio
 import csv
-from fastapi.responses import FileResponse
+
+''' local imports '''
+from src.backend.api_token import APItoken as API_TOKEN 
+
 
 app = FastAPI()
 
 # Allow CORS for local development
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_origins = ["*"],
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"],  
 )
 
 class Options:
-    def __init__(self, api_token):
-        self.api_token = api_token
+    def __init__(self):
+        self.api_token = API_TOKEN
         self.access_token = None
         self.headers = None
-        self.payload = {"api_token": api_token}
+        self.payload = {"api_token": API_TOKEN}
+        self.fieldwire_version = str(dt.now().strftime("%Y-%m-%d")) #date as str
 
-    async def setAccessToken(self, session):
-        url = "https://client-api.super.fieldwire.com/api_keys/jwt"
-        async with session.post(url, json=self.payload, headers={
-            "accept": "application/json",
-            "content-type": "application/json"
-        }) as response:
-            data = await response.json()
-            self.access_token = data['access_token']
-            self.headers = {
+    async def checkAccessToken(self, session):
+        ''' will check the connection or if access_token has expired'''
+        async with session.get("https://client-api.us.fieldwire.com/api/v3/account/account_data_types", headers={
                 "accept": "application/json",
-                "Fieldwire-Version": "2024-05-26",
-                "authorization": f"Bearer {self.access_token}"
-            }
+                "Fieldwire-Version": f"{self.fieldwire_version}",
+                "authorization": f"Bearer {self.access_token}",
+                }) as res:
+            if res.status == 200:
+                return True
+            else:
+                return False
+
+    async def getAccessToken(self, session):
+        url =  'https://client-api.super.fieldwire.com/api_keys/jwt'
+        async with session.post(url, json=self.payload, headers={
+                "accept": "application/json",
+                "content-type": "application/json"}) as response:
+            data = await response.json()
+            return data['access_token']
+            
+    async def setAccessToken(self, session):
+        if self.access_token:
+            if await self.checkAccessToken(session):
+                self.headers = {
+                    "accept": "application/json",
+                    "Fieldwire-Version": f"{self.fieldwire_version}",
+                    "authorization": f"Bearer {self.access_token}",
+                }
+        else:
+            self.access_token = await self.getAccessToken(session)
+            await self.setAccessToken(session) #Probably redundant to rerun this         
 
 class ApiHandler(Options):
-    def __init__(self, api_token):
-        super().__init__(api_token)
+    def __init__(self,):
+        super().__init__()
         self.allProjects = {}
 
     async def setAllProjects(self, session):
@@ -73,6 +97,40 @@ class ApiHandler(Options):
         tasks = [self.getTeamForTask(session, project_id, task['team_id'], task) for task in tasksData]
         return await asyncio.gather(*tasks)
 
+
+@app.get("/projects")
+async def get_projects():
+    async with aiohttp.ClientSession() as session:
+        api = ApiHandler()
+        await api.setAccessToken(session)
+        await api.setAllProjects(session)
+        return [{"id": project_id, "name": project.project_name} for project_id, project in api.allProjects.items()]
+
+@app.get("/project/{project_id}/devices")
+async def get_devices(project_id: str):
+    async with aiohttp.ClientSession() as session:
+        api = ApiHandler()
+        await api.setAccessToken(session)
+        devices = await api.getAllDevicesForProject(session, project_id)
+        return devices
+
+@app.post("/save_csv")
+async def save_csv(devices: list, fields_to_include: dict):
+    header, filename = ["component_labels"], f"component_labels.csv"
+    
+    with open(filename, mode='w', newline='') as file:
+        writer = csv.DictWriter(file, fieldnames = [header])
+        writer.writeheader()
+        
+        for device in devices:
+            component_label = ", ".join(
+                [f"{key}: {value}" for key, value in device.items() if fields_to_include.get(key)]
+            )
+            writer.writerow({"component_label": component_label})
+
+    return FileResponse(filename, media_type='application/octet-stream', filename=filename)
+
+
 class Project:
     def __init__(self, projectProfile):
         self.project_id = projectProfile['id']
@@ -82,43 +140,3 @@ class Project:
         self.team_id = None
         self.team = []
         self.devices = []
-
-@app.post("/get_access_token")
-async def get_access_token(api_token: str):
-    async with aiohttp.ClientSession() as session:
-        api = ApiHandler(api_token)
-        await api.setAccessToken(session)
-        return {"access_token": api.access_token}
-
-@app.get("/projects")
-async def get_projects(api_token: str):
-    async with aiohttp.ClientSession() as session:
-        api = ApiHandler(api_token)
-        await api.setAccessToken(session)
-        await api.setAllProjects(session)
-        return [{"id": project_id, "name": project.project_name} for project_id, project in api.allProjects.items()]
-
-@app.get("/project/{project_id}/devices")
-async def get_devices(api_token: str, project_id: str):
-    async with aiohttp.ClientSession() as session:
-        api = ApiHandler(api_token)
-        await api.setAccessToken(session)
-        devices = await api.getAllDevicesForProject(session, project_id)
-        return devices
-
-@app.post("/save_csv")
-async def save_csv(devices: list, fields_to_include: dict):
-    filename = "component_labels.csv"
-    header = ["component_labels"]
-
-    with open(filename, mode='w', newline='') as file:
-        writer = csv.DictWriter(file, fieldnames=header)
-        writer.writeheader()
-
-        for device in devices:
-            component_label = ", ".join(
-                [f"{key}: {value}" for key, value in device.items() if fields_to_include.get(key)]
-            )
-            writer.writerow({"component_label": component_label})
-
-    return FileResponse(filename, media_type='application/octet-stream', filename=filename)
