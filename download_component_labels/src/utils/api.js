7d5859d5e49b7ccc21abe1ed7155b205d03a1721
Aleksander Borgerød <97392841+Borgerod@users.noreply.github.com>
2024-06-01 13:47:43 +0200

    Update api.js
    - Moved api_token to backend to a separate file.
    - removed unnecessary usage of "softDevices()",
         -  moved  softDevices() to handleSortChange in App.js (where it will only be called when a sort method has been chosen)

--- download_component_labels/src/utils/api.js
+++ download_component_labels/src/utils/api.js
@@ -1,39 +1,27 @@
 import axios from 'axios';
-import { sortDevices } from './sortUtils';
-import { saveCSV } from './csvUtils'; // Ensure saveCSV is imported correctly
-
-//TODO Should Probably change the location of the API token, sort of depends on what the main app looks like.
-// would probably be best to put it in backend
-
-const apiToken = "ewPuywjJJUC89C08gvmqhmPasT5yGJYECmerHfH6TSBwNHctHmB4tEvtV0hbBxW8";
+import { saveCSV } from './csvUtils';
 
 export const fetchProjects = async (setProjects) => {
   try {
-    const response = await axios.get(`http://127.0.0.1:8000/projects?api_token=${apiToken}`);
+    const response = await axios.get(`http://127.0.0.1:8000/projects`);
     setProjects(response.data);
   } catch (error) {
     console.error("Error fetching projects:", error);
   }
 };
 
-export const fetchDevices = async (projectId, setDevices, sortField) => {
+export const fetchDevices = async (projectId, setDevices) => {
   try {
-    const response = await axios.get(`http://127.0.0.1:8000/project/${projectId}/devices?api_token=${apiToken}`);
+    const response = await axios.get(`http://127.0.0.1:8000/project/${projectId}/devices`);
     let devicesData = response.data;
-    if (sortField) {
-      devicesData = sortDevices(devicesData, sortField);
-    }
     setDevices(devicesData);
   } catch (error) {
     console.error("Error fetching devices:", error);
   }
 };
 
-export const fetchDevicesAndSaveCSV = async (projectId, sortField, fields, devices) => {
+export const fetchDevicesAndSaveCSV = async (fields, devices) => {
   try {
-    if (sortField) {
-      devices = sortDevices(devices, sortField);
-    }
     saveCSV(devices, fields);
   } catch (error) {
     console.error("Error fetching devices and saving CSV:", error);
