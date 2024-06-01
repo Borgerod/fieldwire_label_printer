e1b110230c7897b57c8d91f47b78ddc10df0732a
Aleksander Borgerød <97392841+Borgerod@users.noreply.github.com>
2024-06-01 14:47:28 +0200

    Add tooltip

--- download_component_labels/src/components/CloseIconComponent.js
+++ download_component_labels/src/components/CloseIconComponent.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import CloseIcon from '@mui/icons-material/Close';
+import Tooltip from '@mui/material/Tooltip';
 
 const CloseIconComponent = () => {
   return (
     <div className='stack'>
-      <CloseIcon />
+      <Tooltip title="Not in use; will make it once i know wether this widget is supposed to be a window or accordian">
+        <CloseIcon />
+      </Tooltip>
     </div>
   );
 };
