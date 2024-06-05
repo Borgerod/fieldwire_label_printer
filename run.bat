51a41513e0ea98bfdca01b004fc2a18d72f9432d
Aleksander Borgerød <97392841+Borgerod@users.noreply.github.com>
2024-06-05 14:27:02 +0200

    Update run.bat
    add title print
    change comments from REM to ::

--- run.bat
+++ run.bat
@@ -1,17 +1,20 @@
 @echo off
+echo Running FieldWire Label Printer: MacOS/Linux
+echo will run both backend and frontend
+echo. 
 
-REM Check if the current directory is download_component_labels
+:: Check if the current directory is download_component_labels
 for %%I in (.) do if /I "%%~nxI" neq "download_component_labels" (
     cd download_component_labels
 )
 
-REM Activate the virtual environment
+:: Activate the virtual environment
 call .\env\Scripts\activate
 
-REM Start the Uvicorn server
+:: Start the Uvicorn server
 start "Uvicorn Server" cmd /k "python -m uvicorn src.backend.app:app --reload"
 
-REM Start the Node.js application
+:: Start the Node.js application
 start "Node.js Application" cmd /k "npm start"
 
 echo Both the Uvicorn server and Node.js application have been started.
